3508e7711c66864de4661ff1d9050c6294a6c3f4
F88\sangnt3 <sangnt3@f88.vn>
2022-02-18 20:46:16 +0700

    first commit

---
+++ js/site.js
@@ -0,0 +1,12 @@
+// When the DOM is ready, run this function
+$(document).ready(function () {
+  //Set the carousel options
+  $("#featured").carousel({
+    pauseOnHover: true,
+    interval: 5000,
+  });
+  $("#galery").carousel({
+    pauseOnHover: true,
+    interval: 1000,
+  });
+});
